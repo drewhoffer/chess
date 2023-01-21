86fe0b878bf3b5a7d05f7251ffc19f31e22bc3e9
Fresh <drewsdhoffer@gmail.com>
2023-01-20 22:56:07 -0500

    fix typo

--- app/javascript/controllers/chess_controller.js
+++ app/javascript/controllers/chess_controller.js
@@ -1,7 +1,9 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="chess"
 export default class extends Controller {
+  static targets = ["state", "fen"];
   connect() {
+    console.log("connected");
   }
 }
