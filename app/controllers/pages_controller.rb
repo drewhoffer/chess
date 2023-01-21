5136439453fef16cf559760305ed7a070f3e33b6
Fresh <drewsdhoffer@gmail.com>
2023-01-20 22:53:13 -0500

    setup board

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @users = User.where.not(id: current_user)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create!(email: "dean@example.com",
+                    password: "password",
+                    password_confirmation: "password")
+user = User.create!(email: "john@doe.com",
+                    password: "password",
+                    password_confirmation: "password")
+user = User.create!(email: "jane@doe.com",
+                    password: "password",
+                    password_confirmation: "password")
+
+10.times do
+  user = User.create!(email: Faker::Internet.email,
+                      password: "password",
+                      password_confirmation: "password")
+end
